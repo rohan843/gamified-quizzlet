ec47ded2fcc53135a4fa212ac251dd1e6622cdb7
rohan843 <rohan.sharma2934@gmail.com>
2024-03-13 18:51:51 +0530

    fix: add click listener

--- src/pages/GetStartedButton.js
+++ src/pages/GetStartedButton.js
@@ -1,7 +1,14 @@
-function GetStartedButton() {
-  return <button className="get-started-button">
-    Get Started
-  </button>;
+function GetStartedButton({ onClick }) {
+  return (
+    <button
+      className="get-started-button"
+      onClick={(e) => {
+        onClick && onClick(e);
+      }}
+    >
+      Get Started
+    </button>
+  );
 }
 
 export default GetStartedButton;
--- src/pages/TopNav.js
+++ src/pages/TopNav.js
@@ -3,7 +3,9 @@ import GetStartedButton from "./GetStartedButton";
 function TopNav() {
   return (
     <div className="w-full border-b flex flex-row items-center justify-between px-3">
-      <h1 className="text-[#2B7B0F] text-[40px] my-[4px] logo-shadow">Gamified Quizzlet</h1>
+      <h1 className="text-[#2B7B0F] text-[40px] my-[4px] logo-shadow">
+        Gamified Quizzlet
+      </h1>
       <GetStartedButton />
     </div>
   );
