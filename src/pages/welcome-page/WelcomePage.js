19dc667e129fa0c85c517d1327072df6a804ae7a
rohan843 <rohan.sharma2934@gmail.com>
2024-03-13 19:12:59 +0530

    fix: add welcome page markup

--- src/pages/welcome-page/WelcomePage.js
+++ src/pages/welcome-page/WelcomePage.js
@@ -1,9 +1,27 @@
 import TopNav from "./TopNav";
+import robotImage from "../../assets/image 1.png";
 
 function WelcomePage() {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full flex flex-col">
       <TopNav />
+      <div className="flex flex-row grow">
+        <div className="h-full w-[30%]">
+          <img src={robotImage} alt="test" />
+        </div>
+        <div className="flex flex-col h-full grow">
+          <h1 className="text-[#191919] text-[64px]">Learning Starts Here!</h1>
+          <p className="text-[#787878] text-[20px]">
+            The fun and interactive way to enhance your{" "}
+            <span className="text-[#454343] decoration-wavy underline decoration-[#2B7B0F]">
+              General Knowledge
+            </span>
+            .
+          </p>
+          <button>Get Started</button>
+          <button>Login</button>
+        </div>
+      </div>
     </div>
   );
 }
