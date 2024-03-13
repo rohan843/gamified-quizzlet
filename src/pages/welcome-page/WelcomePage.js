2c35d117cb014ad9a2ab053e79d2c7bcd4755cac
rohan843 <rohan.sharma2934@gmail.com>
2024-03-13 22:35:28 +0530

    feat: finish basic welcome page

--- src/pages/welcome-page/WelcomePage.js
+++ src/pages/welcome-page/WelcomePage.js
@@ -5,12 +5,14 @@ function WelcomePage() {
   return (
     <div className="w-full h-full flex flex-col">
       <TopNav />
-      <div className="flex flex-row grow">
-        <div className="h-full w-[30%]">
-          <img src={robotImage} alt="test" />
+      <div className="flex flex-row grow py-10 px-2">
+        <div className="h-full w-[40%] mx-5 flex flex-row justify-center">
+          <div className="w-[220px] h-[260px] object-contain mt-[150px]">
+            <img src={robotImage} alt="test" />
+          </div>
         </div>
         <div className="flex flex-col h-full grow">
-          <h1 className="text-[#191919] text-[64px]">Learning Starts Here!</h1>
+          <h1 className="text-[#191919] text-[64px] mt-[100px]">Learning Starts Here!</h1>
           <p className="text-[#787878] text-[20px]">
             The fun and interactive way to enhance your{" "}
             <span className="text-[#454343] decoration-wavy underline decoration-[#2B7B0F]">
@@ -18,8 +20,8 @@ function WelcomePage() {
             </span>
             .
           </p>
-          <button>Get Started</button>
-          <button>Login</button>
+          <button className="w-max h-max get-started-button mb-3 mt-9">Get Started</button>
+          <button className="w-max h-max login-button">Login</button>
         </div>
       </div>
     </div>
