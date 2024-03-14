b692c886c7bbf62d0238eb90ccc5b40c29d69b26
rohan843 <rohan.sharma2934@gmail.com>
2024-03-14 16:59:24 +0530

    chore: add comment

--- src/pages/dashboard-page/DashboardPage.js
+++ src/pages/dashboard-page/DashboardPage.js
@@ -5,6 +5,7 @@ import { ReactComponent as Heart } from "../../assets/heart.svg";
 function DashboardPage({ streak, gold, lifelines }) {
   return (
     <div className="w-full h-full flex flex-col">
+      {/* Top Nav */}
       <div className="w-full h-max flex flex-row justify-between items-center border-b px-3">
         <h1 className="text-[#2B7B0F] text-[40px] my-[4px] logo-shadow">
           Gamified Quizzlet
