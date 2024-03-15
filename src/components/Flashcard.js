1286ddd463623c6d3fbcc4b6c081012dd41e3a6b
rohan843 <rohan.sharma2934@gmail.com>
2024-03-15 18:30:52 +0530

    chore: auto format and remove warnings

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -1,17 +1,15 @@
-function Flashcard({ name, media, text }) {
+function Flashcard({ name, media, text, onClick }) {
   return (
-    <div className="w-full h-[110px] bg-[#EDF6F9] rounded-[12px] flashcard flex flex-row justify-between py-1 px-1 my-3 border-[#875400] border-[4px]">
+    <div
+      className="w-full h-[110px] bg-[#EDF6F9] rounded-[12px] flashcard flex flex-row justify-between py-1 px-1 my-3 border-[#875400] border-[4px]"
+      onClick={onClick}
+    >
       <div className="h-full w-[90%] flex flex-col justify-between">
-        <span className="text-[50px]">Operating Systems</span>
-        <span className="w-full truncate text-[#A9A9A9]">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse eaque
-          quod aliquam dolorum numquam sequi nam maxime rerum. Odio explicabo
-          officia placeat vel nobis, voluptas fuga dignissimos facere accusamus
-          nostrum!
-        </span>
+        <span className="text-[50px]">{name}</span>
+        <span className="w-full truncate text-[#A9A9A9]">{text}</span>
       </div>
       <div className="w-[10%] h-full max-w-max text-[#A9A9A9] truncate">
-        +Image
+        +{media}
       </div>
     </div>
   );
--- src/pages/dashboard-page/FlashcardsView.js
+++ src/pages/dashboard-page/FlashcardsView.js
@@ -3,12 +3,42 @@ import Flashcard from "../../components/Flashcard";
 function FlashcardsView() {
   return (
     <div className="w-full min-h-max flex flex-col p-10">
-      <Flashcard />
-      <Flashcard />
-      <Flashcard />
-      <Flashcard />
-      <Flashcard />
-      <Flashcard />
+      <Flashcard
+        onClick={() => alert("asdf")}
+        name="Operating Systems"
+        text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero suscipit perferendis autem, earum maxime illum dolorem pariatur porro doloremque asperiores ipsum esse accusantium ad quasi at ex corporis quam in."
+        media="Image"
+      />
+      <Flashcard
+        onClick={() => alert("asdf")}
+        name="Operating Systems"
+        text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero suscipit perferendis autem, earum maxime illum dolorem pariatur porro doloremque asperiores ipsum esse accusantium ad quasi at ex corporis quam in."
+        media="Image"
+      />
+      <Flashcard
+        onClick={() => alert("asdf")}
+        name="Operating Systems"
+        text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero suscipit perferendis autem, earum maxime illum dolorem pariatur porro doloremque asperiores ipsum esse accusantium ad quasi at ex corporis quam in."
+        media="Image"
+      />
+      <Flashcard
+        onClick={() => alert("asdf")}
+        name="Operating Systems"
+        text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero suscipit perferendis autem, earum maxime illum dolorem pariatur porro doloremque asperiores ipsum esse accusantium ad quasi at ex corporis quam in."
+        media="Image"
+      />
+      <Flashcard
+        onClick={() => alert("asdf")}
+        name="Operating Systems"
+        text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero suscipit perferendis autem, earum maxime illum dolorem pariatur porro doloremque asperiores ipsum esse accusantium ad quasi at ex corporis quam in."
+        media="Image"
+      />
+      <Flashcard
+        onClick={() => alert("asdf")}
+        name="Operating Systems"
+        text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero suscipit perferendis autem, earum maxime illum dolorem pariatur porro doloremque asperiores ipsum esse accusantium ad quasi at ex corporis quam in."
+        media="Image"
+      />
     </div>
   );
 }
